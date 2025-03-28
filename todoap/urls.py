656bb444a2d79bbc3e72a9df9dab8d94eb515523
Agbar102 <kamchiagbar@gmail.com>
2025-03-29 01:27:33 +0600

    static

--- todoap/urls.py
+++ todoap/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todolist.urls')),
-    path('main', include('main.urls'))
+    path('main', include('main.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
